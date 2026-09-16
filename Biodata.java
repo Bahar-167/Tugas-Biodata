56c6a9177ef388cd620f0b8676fee3db6bae4708
M. Baharudin Wakhid <varie0167@gmail.com>
2026-09-16 11:07:52 +0700

    Update Biodata.java

--- Biodata.java
+++ Biodata.java
@@ -1,7 +1,7 @@
 public class Biodata {
     public static void main(String[] args) {
         System.out.println("Nama     : M. BAHARUDIN WAKHID");
-        System.out.println("NIM      : 123456789");
+        System.out.println("NIM      : 261301019");
         System.out.println("Jurusan  : Sistem Komputer");
         System.out.println("Universitas: Universitas Nahdlatul Ulama");
     }
